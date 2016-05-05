726bc6f225eb193ae7d98a583b16fa2776f6fd32
Brad-- <brad.smith.1324@gmail.com>
2016-05-04 20:41:36 -0600

    Made a few barebones files, maybe using sass because it's cool :fish:

---
+++ public/js/app.js
@@ -0,0 +1,11 @@
+( function(){
+	var app = angular
+		.module('MainCtrl', []);
+
+	app.controller('MainCtrl', MainCtrl)
+	function MainCtrl() {
+		var self = this;
+	}
+	
+// End namespace wrap
+})();
